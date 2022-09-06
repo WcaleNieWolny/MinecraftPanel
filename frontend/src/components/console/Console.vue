8801315432b5db1fdfb859c15c7a231041288af3
wolny <isupermichael007@gmail.com>
2022-09-06 19:09:00 +0200

    Setup monorepo, start working on frontend

---
+++ frontend/src/components/console/Console.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>Console Window</h1>
+        <Input text="Add Tasks"/>
+    </header>  
+</template>
+
+
+<script>
+import Input from './Input.vue'
+
+export default {
+    name: 'Console',
+
+    components: {
+        Input,
+    },
+}
+</script>
+
+<style scoped>
+    header{
+        display: inline;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ frontend/src/components/console/Input.vue
@@ -0,0 +1,27 @@
+<template>
+    <input v-model="formdata.input" placeholder="minecraft command" />
+    <button @click="onClick()">{{ text }}</button>
+</template>
+
+<script lang="ts">
+    export default{
+        name: 'Input',
+
+        data() {
+            return {
+                formdata: { input: ''}
+            }
+        },
+
+        props: {
+            text: String,
+        },
+
+        methods: {
+            onClick() {
+                console.log(this.$data.formdata.input)
+                this.$data.formdata.input = ""
+            },
+        }
+    }
+</script>
